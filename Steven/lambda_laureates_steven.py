b0ea859add354da14e33d7c55215f9b634b7184d
StevenLTD <108654827+StevenLTD@users.noreply.github.com>
2022-08-08 17:24:01 +0200

    Add files via upload

---
+++ Steven/lambda_laureates_steven.py
@@ -0,0 +1,51 @@
+import json
+import requests
+import pandas as pd
+import boto3
+def lambda_handler(event, context):
+
+    url = 'http://api.nobelprize.org/2.1/laureates?ID={}'
+
+
+    # #TODO Pasar parametro por API
+    laureate_id = event['queryStringParameters']['id']
+
+    info_laureate = requests.get(url.format(laureate_id))
+
+    resultApi=info_laureate.json()
+
+    laureateDict=resultApi['laureates'][0]
+
+    if 'gender' in laureateDict:
+        fullName=laureateDict['fullName']['en']
+        fileName=laureateDict['fileName']
+        gender=laureateDict['gender']
+        birth=laureateDict['birth']['date']
+    else:
+        fullName='None'
+        fileName=laureateDict['fileName']
+        gender='None'
+        birth='None'
+
+    dfParquet=pd.DataFrame(columns=['id','fullName','fileName','gender','birth'])
+
+    dfParquet=dfParquet.append({
+
+        'id':laureateDict['id'],
+        'fullName': fullName,
+        'fileName':fileName,
+        'gender':gender,
+        'birth':birth
+
+    } ,ignore_index=True)
+
+
+    s3=boto3.client('s3')
+
+    content=dfParquet.to_parquet()
+    prueba=dfParquet.to_json()
+    s3.put_object(Body=content, Bucket='alvaro-munoz-nobelprice', Key='raw/laureate/'+ str(laureate_id)  +'.parquet')
+    return {
+        'statusCode': 200,
+        'body': prueba
+    }
---
+++ Steven/lambda_nobel_steven.py
@@ -0,0 +1,58 @@
+import json
+import requests
+import pandas as pd
+import boto3
+
+def lambda_handler(event, context):
+    event_category = event["queryStringParameters"]["category"]
+    event_year = event["queryStringParameters"]["year"]
+    url = 'http://api.nobelprize.org/2.1/nobelPrize/{}/{}'
+
+    resultApi = requests.get(url.format(event_category,event_year))
+
+    laureates_id=[]
+    laureates_portion=[]
+    resultApi=resultApi.json()
+    resultDict=resultApi[0]
+
+    try:
+        prueba2=resultDict['laureates']
+        dateAwarded=resultDict['dateAwarded']
+        for i in range(len(prueba2)):
+            laureates_id.append(prueba2[i]['id'])
+            laureates_portion.append(prueba2[i]['portion'])
+    print(arrId)
+    except:
+        laureates_id=None
+        dateAwarded=None
+        laureates_portion=None
+
+    dfNobelPrize = pd.DataFrame(columns=['awardYear','category','categoryFullName','dateAwarded','prizeAmount','prizeAmountAdjusted','id','portion'] )
+
+
+    dfNobelPrize=dfNobelPrize.append({
+
+                'id': laureates_id,
+                'portion': laureates_portion,
+                'awardYear': resultDict['awardYear'],
+                'category': resultDict['category']['en'],
+                'categoryFullName': resultDict['categoryFullName']['en'],
+                'dateAwarded': dateAwarded,
+                'prizeAmount': resultDict['prizeAmount'],
+                'prizeAmountAdjusted': resultDict['prizeAmountAdjusted']
+
+            }, ignore_index=True)
+
+    dfNobelPrize=dfNobelPrize.set_index('id')
+
+
+
+    s3=boto3.client('s3')
+
+    content=dfNobelPrize.to_parquet()
+    s3.put_object(Body=content, Bucket='alvaro-munoz-nobelprice', Key='raw/nobelPrize/'+ event_category +'-'+ str(event_year)  +'.parquet')
+
+  return {
+      'statusCode': 200,
+      'body': json.dumps(event)
+  }
